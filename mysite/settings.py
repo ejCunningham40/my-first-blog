8e9285cf22f4e460ba36a58276c36d616380b8f3
Ethan Cunningham <ethan@sunrise.co.za>
2025-02-28 16:43:51 +0200

    Add wagtail to blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,19 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "wagtail.contrib.forms",
+    "wagtail.contrib.redirects",
+    "wagtail.embeds",
+    "wagtail.sites",
+    "wagtail.users",
+    "wagtail.snippets",
+    "wagtail.documents",
+    "wagtail.images",
+    "wagtail.search",
+    "wagtail.admin",
+    "wagtail",
+    "modelcluster",
+    "taggit",
     "blog",
 ]
 
@@ -48,6 +61,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
 
 ROOT_URLCONF = "mysite.urls"
@@ -118,7 +132,20 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
+WAGTAIL_SITE_NAME = "Django Girls Blog"
+WAGTAILADMIN_BASE_URL = "http://127.0.0.1:8000"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGIN_REDIRECT_URL = "/blog/"
+# settings.py
+LOGOUT_REDIRECT_URL = "/blog/"
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views
+
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail import urls as wagtail_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
-]
+    path("accounts/login/", views.LoginView.as_view(), name="login"),
+    path(
+        "accounts/logout/", views.LogoutView.as_view(next_page="/blog/"), name="logout"
+    ),
+    path("blog/", include("blog.urls")),
+    path("cms/", include(wagtailadmin_urls)),
+    path("", include(wagtail_urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
